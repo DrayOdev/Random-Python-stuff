44d73ebe24b413457b40b9491958a35e543e3b21
DrayOdev <dreaorealacc@gmail.com>
2025-09-26 00:54:52 +0100

    Update NewOdditiesVideo_No_Comments.py

--- NewOdditiesVideo_No_Comments.py
+++ NewOdditiesVideo_No_Comments.py
@@ -1,17 +1,18 @@
-import webbrowser, datetime, time
+import requests, re, webbrowser, datetime, time
 
-UPLOAD_HOUR: int = 20 
-UPLOAD_MINUTE: int = 0 
+UPLOAD_HOUR: int = 0
+UPLOAD_MINUTE: int = 53
 CHANNEL_URL: str = "https://youtube.com/@theoddities_roleplay"
 
 def Main():
     while True:
         now = datetime.datetime.now()
         if now.hour == UPLOAD_HOUR and now.minute == UPLOAD_MINUTE:
-            webbrowser.open(CHANNEL_URL) 
+            html = requests.get(CHANNEL_URL + "/videos").text
+            url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+            webbrowser.open(url)
             break
         else:
             time.sleep(60 - now.second)
 
 Main()
-
