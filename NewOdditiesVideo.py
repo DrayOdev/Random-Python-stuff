55bffa431f40482ce425221df14e7914340247b7
DrayOdev <dreaorealacc@gmail.com>
2025-09-25 23:00:26 +0100

    NewOdditiesVideo.py
    Minor changes :D

--- NewOdditiesVideo.py
+++ NewOdditiesVideo.py
@@ -5,32 +5,29 @@ import datetime
 import time
 
 UPLOAD_HOUR = 20 # Change me to fit the upload schedule :D
-UPLOAD_MINUTE = 0 # and me as well!
-CHANNEL_URL = "https://youtube.com/@theoddities_roleplay" # Change for any channel you like
+UPLOAD_MINUTE = 0 # me as well!
+CHANNEL_URL = "https://youtube.com/@theoddities_roleplay"  # Change for any channel you like
 
-def run():
-    new_upload: bool = False
+def Main():
+    new_upload: bool = False # creates the varible that the loop is based from
     while new_upload == False:
-        if datetime.datetime.now().hour == UPLOAD_HOUR and datetime.datetime.now().minute == UPLOAD_MINUTE: # checks to see if it is the scheduled time for an upload
+        now = datetime.datetime.now() # DO NOT REMOVE IM VERY IMPORTANT :D
+        if now.hour == UPLOAD_HOUR and now.minute == UPLOAD_MINUTE: # checks to see if it is the scheduled time for an upload
             webbrowser.open(CHANNEL_URL)
             new_upload = True # kills the loop, alternitevly remove me if you want this loop to go on forever and ever and ever.
         else:
-            now = datetime.datetime.now() # resets time for the print can be removed
-            print("NO NEW UPLOAD", now.time()) # so can i! if you dont want me spamming the console all day!
-            time.sleep(60) # delays the loop by 60s
+            print("NO NEW UPLOAD", now.time()) # Uh well, im only used for debugging so uh feel free to remove me :D
+            time.sleep(60) # i delay the loop by 60s
 
-def start(): # whole thing is for syncing the script to the second (NOT MILLISECOND but i could totally do that.)
+def start(): # whole thing is for syncing the script to the second. Syncing to the microsecond would require this to be a script that is ran 24/7 as it takes a while for it to sync
     start_loop: bool = False # creates the variable that the loop is based from
     while start_loop == False:
         now = datetime.datetime.now()
         if now.second == 0:
-            print("STARTING") # Im not required 
+            print("STARTING") # im not required!
             start_loop = True # kills the loop!
-            run() # calls the main part of the script could have called this main instead of run!
+            Main() # Calls the Main function
         else:
             print("Loop failed at: ", datetime.datetime.now().time(), "\n Attempting again in 1s") # You dont need me!
             time.sleep(1)
-start() # kickstarts the script :D
-
-
-
+start() #Starts the script :D
