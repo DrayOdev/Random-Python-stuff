574446eb5e5fbdffffa19995c2803ea2ab1fd4a2
DrayOdev <dreaorealacc@gmail.com>
2025-09-25 22:07:01 +0100

    NewOdditiesVideo.py
    Added comments to some lines of code to explain stuff :D

--- NewOdditiesVideo.py
+++ NewOdditiesVideo.py
@@ -1,18 +1,19 @@
+# This is a simple script i made that checks (in prolly a really dumb way) if the time is 20:00:00 BST which is the time you can expect a new video from the oddities roleplay :D
 import webbrowser
 import datetime
 import time
 
 def run():
-    if datetime.datetime.now().hour == 20 and datetime.datetime.now().minute == 0:
-        webbrowser.open("https://youtube.com/@theoddities_roleplay")
-    else:
-        print("NO NEW UPLOAD")
-        time.sleep(60)
+    if datetime.datetime.now().hour == 20 and datetime.datetime.now().minute == 0: # checks if its upload time
+        webbrowser.open("https://youtube.com/@theoddities_roleplay") # opens their channel
+    else: # not the time for an upload
+        print("NO NEW UPLOAD") 
+        time.sleep(60) # waits 60s before calling the run() function
         run()
 
-def start():
+def start(): # do i really need to add comments to all this?
     start_loop: bool = False
-    if datetime.datetime.now().second == 0 and start_loop == False:
+    if datetime.datetime.now().second == 0 and start_loop == False: # so the script syncs with real time and so line 11 wont cause issues due to desync 
         print("STARTING")
         start_loop = True
         run()
@@ -21,3 +22,4 @@ def start():
         time.sleep(1)
         start()
 start()
+
