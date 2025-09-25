409dfc1161714165beb721bee853a9a9b2c30dd1
DrayOdev <dreaorealacc@gmail.com>
2025-09-26 00:56:47 +0100

    Update NewOdditiesVideo.py

--- NewOdditiesVideo.py
+++ NewOdditiesVideo.py
@@ -1,19 +1,20 @@
 # This is a simple script i made that checks if the time is 20:00:00* BST which is the time you can expect a new video from the oddities roleplay* :D
 # * <- means that this part can be easily changed by YOU!
-import webbrowser, datetime, time
+import requests, re, webbrowser, datetime, time
 
 UPLOAD_HOUR: int = 20 # Change me to fit the upload schedule :D
 UPLOAD_MINUTE: int = 0 # me as well!
-CHANNEL_URL: str = "https://youtube.com/@theoddities_roleplay"  # Change for any channel you like
+CHANNEL_URL: str = "https://youtube.com/@theoddities_roleplay" # Change for any channel you like
 
 def Main():
     while True:
         now = datetime.datetime.now()
         if now.hour == UPLOAD_HOUR and now.minute == UPLOAD_MINUTE: # if it is the scheduled time for an upload
-            webbrowser.open(CHANNEL_URL) 
+            html = requests.get(CHANNEL_URL + "/videos").text
+            url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+            webbrowser.open(url)
             break # kills the loop
         else:
-            time.sleep(60 - now.second) # i delay the loop by 60s
+            time.sleep(60 - now.second)
 
-Main() #Starts the script :D
-
+Main() # Starts the script :D
