1dfef97eafaba1b95628dbe8ebfdb9f8125ed863
DrayOdev <dreaorealacc@gmail.com>
2025-09-25 22:43:44 +0100

    NewOdditiesVideo.py
    Explained it more depth! also NO MORE RECCURSION! <- i dont wanna talk about this XD
    
    I explain how you can change it to fit anyone else daily upload schedule (limited to one video a day as of right now) and what you can/cant remove. Overall made it more user friendly :D

--- NewOdditiesVideo.py
+++ NewOdditiesVideo.py
@@ -1,25 +1,36 @@
-# This is a simple script i made that checks (in prolly a really dumb way) if the time is 20:00:00 BST which is the time you can expect a new video from the oddities roleplay :D
+# This is a simple script i made that checks if the time is 20:00:00* BST which is the time you can expect a new video from the oddities roleplay* :D
+# * <- means that this part can be easily changed by YOU!
 import webbrowser
 import datetime
 import time
 
-def run():
-    if datetime.datetime.now().hour == 20 and datetime.datetime.now().minute == 0: # checks if its upload time
-        webbrowser.open("https://youtube.com/@theoddities_roleplay") # opens their channel
-    else: # not the time for an upload
-        print("NO NEW UPLOAD") 
-        time.sleep(60) # waits 60s before calling the run() function
-        run()
+UPLOAD_HOUR = 20 # Change me to fit the upload schedule :D
+UPLOAD_MINUTE = 0 # and me as well!
+CHANNEL_URL = "https://youtube.com/@theoddities_roleplay" # Change for any channel you like
 
-def start(): # do i really need to add comments to all this?
-    start_loop: bool = False
-    if datetime.datetime.now().second == 0 and start_loop == False: # so the script syncs with real time and so line 11 wont cause issues due to desync 
-        print("STARTING")
-        start_loop = True
-        run()
-    elif start_loop == False:
-        print("Loop failed trying again in 1s")
-        time.sleep(1)
-        start()
-start()
+def run():
+    new_upload: bool = False
+    while new_upload == False:
+        if datetime.datetime.now().hour == UPLOAD_HOUR and datetime.datetime.now().minute == UPLOAD_MINUTE: # checks to see if it is the scheduled time for an upload
+            webbrowser.open(CHANNEL_URL)
+            new_upload = True # kills the loop, alternitevly remove me if you want this loop to go on forever and ever and ever.
+        else:
+            now = datetime.datetime.now() # resets time for the print can be removed
+            print("NO NEW UPLOAD", now.time()) # so can i! if you dont want me spamming the console all day!
+            time.sleep(60) # delays the loop by 60s
+
+def start(): # whole thing is for syncing the script to the second (NOT MILLISECOND but i could totally do that.)
+    start_loop: bool = False # creates the variable that the loop is based from
+    while start_loop == False:
+        now = datetime.datetime.now()
+        if now.second == 0:
+            print("STARTING") # Im not required 
+            start_loop = True # kills the loop!
+            run() # calls the main part of the script could have called this main instead of run!
+        else:
+            print("Loop failed at: ", datetime.datetime.now().time(), "\n Attempting again in 1s") # You dont need me!
+            time.sleep(1)
+start() # kickstarts the script :D
+
+
 
