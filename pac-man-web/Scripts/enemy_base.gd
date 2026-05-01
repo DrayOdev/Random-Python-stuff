bca6d522350b7e374f691eb5cad0d8d3d667de98
DrayOdev <dreaorealacc@gmail.com>
2026-05-01 14:26:48 +0100

    Add raycasting for PacMan detection in enemy script

--- pac-man-web/Scripts/enemy_base.gd
+++ pac-man-web/Scripts/enemy_base.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 class_name Blinky
 
+@onready var ray: RayCast2D = $ray
 @export var movSpeed: float = 12
 @onready var nav: NavigationAgent2D = $Nav
 @export var threshold: float = 25
 @export var allPoints: Array[Marker2D] = []
 @export var preferedPoints: Array[Marker2D] = []
+@export var PacMan: player
+var SeenPac: bool = false
 var prefered_chance: float = 45.0
 var in_loop: bool = false
 var val: int = 0:
@@ -16,6 +19,11 @@ func _ready() -> void:
 	_upd_next_loc(allPoints[randi_range(0,67)].global_transform.origin)
 
 func _physics_process(delta: float) -> void:
+	var target = -(global_transform.origin - PacMan.global_transform.origin)
+	ray.target_position = target
+	ray.force_raycast_update()
+	$Label.text = str(SeenPac)
+	
 	var cur_location: Vector2 = global_transform.origin
 	var next_location: Vector2 = nav.get_next_path_position()
 	var new_vel = (next_location - cur_location).normalized() * movSpeed
@@ -30,6 +38,12 @@ func _upd_next_loc(target_loc:Vector2) -> Vector2:
 	return nav.get_next_path_position()
 
 func _on_nav_target_reached() -> void:
+	if !ray.get_collider():
+		_on_player_detect_body_entered(PacMan)
+	if SeenPac:
+		_upd_next_loc(PacMan.global_transform.origin)
+		return
+	print("a")
 	if in_loop:
 		val += 1
 		_upd_next_loc(preferedPoints[val].global_transform.origin)
@@ -45,3 +59,15 @@ func _on_nav_target_reached() -> void:
 func die():
 	## return to spawn transform
 	pass
+
+func _on_player_detect_body_entered(body: Node2D) -> void:
+	if ray.is_colliding():
+		SeenPac = false
+	else:
+		SeenPac = true
+		_upd_next_loc(PacMan.global_transform.origin)
+	
+
+
+func _on_player_detect_body_exited(body: Node2D) -> void:
+	SeenPac = false
